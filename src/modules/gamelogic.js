a2d693deb2b3ba7b03dacb73cfbd42f91d7f9ede
HyunCafe <eparksuncafe@gmail.com>
2023-05-20 14:57:34 -0500

    Add: Added computer ship creation test

--- src/modules/gamelogic.js
+++ src/modules/gamelogic.js
@@ -37,5 +37,6 @@ export const startGame = () => {
   playGame();
   return {
     playerShips: [carrier, battleship, destroyer, submarine, patrolBoat],
+    computerShips: [carrier, battleship, destroyer, submarine, patrolBoat],
   };
 };
--- src/tests/gamelogic.test.js
+++ src/tests/gamelogic.test.js
@@ -3,6 +3,8 @@ import { gameBoard } from "../modules/gameboard";
 import { startGame } from "../modules/gamelogic";
 
 describe("startGame", () => {
+    
+  // Player Ship Creation Test
   test("Correctly create the player Carrier ship", () => {
     const { playerShips } = startGame();
     const carrier = playerShips.find((ship) => ship.shipName === "Carrier");
@@ -52,7 +54,59 @@ describe("startGame", () => {
     expect(typeof patrolBoat.isSunk).toBe("function");
   });
 
-  test("Correctly create the computers ships", () => {});
+  // Computer Ship Creation Test
+  test("Correctly create the computer's Carrier ship", () => {
+    const { computerShips } = startGame();
+    const carrier = computerShips.find((ship) => ship.shipName === "Carrier");
+    expect(carrier).toBeDefined();
+    expect(carrier.shipLength).toBe(5);
+    expect(typeof carrier.hit).toBe("function");
+    expect(typeof carrier.isSunk).toBe("function");
+  });
+
+  test("Correctly create the computer's Battleship", () => {
+    const { computerShips } = startGame();
+    const battleShip = computerShips.find(
+      (ship) => ship.shipName === "Battleship"
+    );
+    expect(battleShip).toBeDefined();
+    expect(battleShip.shipLength).toBe(4);
+    expect(typeof battleShip.hit).toBe("function");
+    expect(typeof battleShip.isSunk).toBe("function");
+  });
+
+  test("Correctly create the computer's Destroyer ship", () => {
+    const { computerShips } = startGame();
+    const destroyer = computerShips.find(
+      (ship) => ship.shipName === "Destroyer"
+    );
+    expect(destroyer).toBeDefined();
+    expect(destroyer.shipLength).toBe(3);
+    expect(typeof destroyer.hit).toBe("function");
+    expect(typeof destroyer.isSunk).toBe("function");
+  });
+
+  test("Correctly create the computer's Submarine", () => {
+    const { computerShips } = startGame();
+    const submarine = computerShips.find(
+      (ship) => ship.shipName === "Submarine"
+    );
+    expect(submarine).toBeDefined();
+    expect(submarine.shipLength).toBe(3);
+    expect(typeof submarine.hit).toBe("function");
+    expect(typeof submarine.isSunk).toBe("function");
+  });
+
+  test("Correctly create the computer's Patrol Boat", () => {
+    const { computerShips } = startGame();
+    const patrolBoat = computerShips.find(
+      (ship) => ship.shipName === "Patrol Boat"
+    );
+    expect(patrolBoat).toBeDefined();
+    expect(patrolBoat.shipLength).toBe(2);
+    expect(typeof patrolBoat.hit).toBe("function");
+    expect(typeof patrolBoat.isSunk).toBe("function");
+  });
 
   test("Correctly registers players attacks", () => {});
 
